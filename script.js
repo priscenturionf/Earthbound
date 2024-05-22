407bef7aa9bbcdfea3d616f33dcdb6a9def8cad9
Priscila Centurion <167469576+BloodySulf@users.noreply.github.com>
2024-05-22 03:36:38 -0300

    script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function cerrarPopup() {
 document.addEventListener("DOMContentLoaded", function() {
     abrirPopup();
 });
+
+const motherSound = document.getElementById("mothersound");
+
+const music = document.querySelectorAll('.close-btn');
+
+motherSound.volume = 0.1;
+
+music.forEach(music => {
+    music.addEventListener('click', () => {
+        motherSound.play();
+    });
+});
